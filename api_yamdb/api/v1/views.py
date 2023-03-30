e0ef121544199b2765c6d6e7f9540e75ce92a815
Александр Матвеев <admatveev17@gmail.com>
2023-03-30 15:22:23 +0300

    models edit

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from serializers import CommentSerializer, ReviewSerializer
 from rest_framework.pagination import LimitOffsetPagination
 
+
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     pagination_class = LimitOffsetPagination
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,13 +4,19 @@ from constants import MIN_SCORE, MAX_SCORE
 
 
 class Review(models.Model):
-    title = (
-        ... #  models.ForeignKey(Title, on_delete=models.CASCADE, related_name='reviews', verbose_name='Название произведения')
-    )  
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        verbose_name="Название произведения",
+    )
     text = models.TextField(verbose_name="Отзыв")
-    author = (
-        ... #  models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews', verbose_name='Автор')
-    )  
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        verbose_name="Автор",
+    )
     score = models.IntegerField(
         verbose_name="Оценка",
         validators=[
@@ -22,6 +28,7 @@ class Review(models.Model):
         "Дата добавления", auto_now_add=True, db_index=True
     )
 
+
 class Comment(models.Model):
     review = models.ForeignKey(
         Review,
@@ -30,9 +37,12 @@ class Comment(models.Model):
         related_name="comments",
     )
     text = models.TextField(verbose_name="Комментарий")
-    author = (
-        ... #  models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews', verbose_name='Автор')
-    )  
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        verbose_name="Автор",
+    )
     pub_date = models.DateTimeField(
         "Дата добавления", auto_now_add=True, db_index=True
     )
