ca8f02c7c6733788cfd57f0f6a2cd44129cf72d5
Александр Матвеев <admatveev17@gmail.com>
2023-03-30 13:14:09 +0300

    first part

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from constants import MIN_SCORE, MAX_SCORE
 
-# Create your models here.
+
+class Review(models.Model):
+    text = models.TextField(verbose_name="Текст отзыва")
+    author = ...
+    score = models.IntegerField(verbose_name="Оценка", 
+    validators=[MinValueValidator(MIN_SCORE), MaxValueValidator(MAX_SCORE)])
+    pub_date = models.DateTimeField("Дата добавления", auto_now_add=True, db_index=True)
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name="Текст отзыва")
+    author = ...
+    pub_date = models.DateTimeField("Дата добавления", auto_now_add=True, db_index=True)
