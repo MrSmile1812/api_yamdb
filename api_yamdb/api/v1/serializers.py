759fbb290083655609d98f008cc00dddc90a3835
Александр Матвеев <admatveev17@gmail.com>
2023-04-02 21:41:50 +0300

    Prototype

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -11,7 +11,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     )
 
     def validate(self, data):
-        """Валидируем, что на одно произведение 
+        """Валидируем, что на одно произведение
         пользователь может оставить только один отзыв."""
         request = self.context.get("request")
         if request.method == "POST":
